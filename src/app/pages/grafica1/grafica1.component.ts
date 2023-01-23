07b9a2bea2968170f0591035c42be8b516822465
Julio Saul <juliosaul12@gmail.com>
2023-01-23 12:40:13 -0400

    - Se agrego el diseño del html en el componente de grafica1 y progress. ' - Se agrego la logica que conecta la data el componenete hijo en los archivos ts del componente progress y grafica1. -

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { ChartData } from 'chart.js';
+
 
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
-  styles: [
-  ]
+  styleUrls: []
 })
 export class Grafica1Component {
 
+  public labels1: string[] = ['Pan', 'Refresco', 'Tacos'];
+  public data1: ChartData<'doughnut'> ={
+    labels: this.labels1,
+    datasets: [
+      {
+        data: [10, 450, 100]
+      }
+    ]
+  };
 }
