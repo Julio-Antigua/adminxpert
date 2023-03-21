5ba05b684bb0cd6bfbfcaec567813ac5385bee80
INTECADM\julio.antigua <julio.antigua@intec.edu.do>
2023-03-21 12:29:53 -0400

    - Se agrego el RouterModule al al shared.module.ts -

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -12,13 +13,14 @@ import { HeaderComponent } from './header/header.component';
     SidebarComponent,
     HeaderComponent
   ],
-  imports: [
-    CommonModule
-  ],
   exports: [
     BreadcrumbsComponent,
     SidebarComponent,
     HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
   ],
 })
 export class SharedModule { }
