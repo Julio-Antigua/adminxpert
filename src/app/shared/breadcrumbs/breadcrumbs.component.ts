13bd80682f23ba937176a53e7ab4cd69c38555b2
INTECADM\julio.antigua <julio.antigua@intec.edu.do>
2023-03-23 14:57:25 -0400

    - Se creo la logica para agregar el titulo de la pagina donde estemos en el breadcrumbs y se implemento la logica en el html. -

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from "@angular/router";
+import { map, Subscription } from 'rxjs';
+import { filter } from "rxjs/internal/operators/filter";
+
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,6 +10,35 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy{
 
+  public title!: string;
+  public titleSubs$: Subscription;
+
+  constructor(private router: Router) {
+    this.titleSubs$ = this.getArgumentsRouter()
+              .subscribe( ({title}) => { 
+              console.log(title);
+              this.title = title;
+              document.title = `AdminXpert - ${title}`;
+            });
+  }
+  
+  ngOnDestroy(): void {
+    this.titleSubs$.unsubscribe();
+  }
+
+  getArgumentsRouter(){
+
+    return this.router.events
+    .pipe(
+      filter( event => event instanceof ActivationEnd ),
+      filter( (event: any) =>  event.snapshot.firstChild === null ),
+      map( (event: any) =>  event.snapshot.data )
+    );
+    
+  }
+
+
+  
 }
